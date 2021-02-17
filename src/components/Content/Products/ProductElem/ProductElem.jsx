844711018c69177df0f671a26a5b53afc3b49128
Irina <i.lashina@cometrica.ru>
2021-02-17 18:34:25 +0300

    s lubovui

--- src/components/Content/Products/ProductElem/ProductElem.jsx
+++ src/components/Content/Products/ProductElem/ProductElem.jsx
@@ -3,49 +3,58 @@ import SwitchersPizza from '../SwitchersPizza/SwitchersPizza';
 import s from './../Products.module.css';
 
 const ProductElem = (props) => {
-	const amount = props.basket.amountElem;
-	const sum = props.basket.sum;
-	const onAddElemToBasket = (elem, price) => {
-		props.updateBasket(elem, amount + 1, sum + price);
-	};
+  const amount = props.basket.amountElem;
+  const sum = props.basket.sum;
+  const onAddElemToBasket = (elem, price) => {
+    props.updateBasket(elem, amount + 1, sum + price);
+  };
 
-/*     const [sizePizza, setSizePizza] = useState(true); */
+  console.log(props.products[0].finalPrice, 'ЦЕНА');
 
-	return (
-		<>
-			{props.products.map((elem) => (
-				<div key={elem.id} className={s.productBlock}>
-					<img src={elem.image} alt="product" className={s.photo} />
-					<div className={s.productContent}>
-						<div>
-							<div className={s.productInfo}>
-								<h3 className={s.productName}>{elem.name}</h3>
-								<span className={s.weight}>{elem.weightThinS || elem.weight}г</span>
-							</div>
-							<div className={s.description}>{elem.composition}</div>
-						</div>
+  /*     const [sizePizza, setSizePizza] = useState(true); */
 
-						{props.indicatorPizza ? <SwitchersPizza setSizePizza={props.setSizePizza} id={elem.id} /* setSizePizza={setSizePizza} */  /> : null}
+  return (
+    <>
+      {props.products.map((elem) => (
+        <div key={elem.id} className={s.productBlock}>
+          <img src={elem.image} alt="product" className={s.photo} />
+          <div className={s.productContent}>
+            <div>
+              <div className={s.productInfo}>
+                <h3 className={s.productName}>{elem.name}</h3>
+                <span className={s.weight}>
+                  {elem.weightThinS || elem.weight}г
+                </span>
+              </div>
+              <div className={s.description}>{elem.composition}</div>
+            </div>
 
-						<div className={s.registration}>
-							<span className={s.price}>
-                   {/*             { sizePizza ? elem.priceSizeSmall : elem.priceSizeMiddle } &#8381; */}
-							{elem.priceSizeSmall || elem.price} &#8381; 
-							</span>
-							<button
-								className={s.btnAdd}
-								onClick={() => {
-									onAddElemToBasket(elem, elem.price);
-								}}
-							>
-								В корзину
-							</button>
-						</div>
-					</div>
-				</div>
-			))}
-		</>
-	);
+            {props.indicatorPizza ? (
+              <SwitchersPizza
+                setSizePizza={props.setSizePizza}
+                id={elem.id} /* setSizePizza={setSizePizza} */
+              />
+            ) : null}
+
+            <div className={s.registration}>
+              <span className={s.price}>
+                {/*             { sizePizza ? elem.priceSizeSmall : elem.priceSizeMiddle } &#8381; */}
+                {elem.priceSizeSmall || elem.price} &#8381;
+              </span>
+              <button
+                className={s.btnAdd}
+                onClick={() => {
+                  onAddElemToBasket(elem, elem.price);
+                }}
+              >
+                В корзину
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default ProductElem;
